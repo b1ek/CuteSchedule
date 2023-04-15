127653da002bc7057d5916b443c0f08f5a70e64b
b1ek <me@blek.codes>
2023-04-16 02:19:07 +1000

    add platform check

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
-#include "src/mainwindow.h"
+
+#ifndef _WIN32
+#ifndef linux
+
+#error "Can't recognize your platform. Please either set _WIN32 or 'linux' macro according to your system."
+
+#endif
+#endif
 
 #include <QApplication>
-#include <iostream>
 
-#include "yaml-cpp/yaml.h"
+#include "./src/config/config.h"
+#include "src/mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    Config config;
+
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+
     return a.exec();
 }
